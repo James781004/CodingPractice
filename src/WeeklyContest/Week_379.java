f7c053f6752553d29e8066f65dd2556544136b21
James Huang <James781004@hotmail.com>
2024-01-07 21:56:57 +0800

    Week 379

--- src/WeeklyContest/Week_379.java
+++ src/WeeklyContest/Week_379.java
@@ -1,6 +1,8 @@
 package WeeklyContest;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Week_379 {
@@ -71,4 +73,51 @@ public class Week_379 {
         return ans;
     }
 
+
+    // https://leetcode.cn/problems/maximize-the-number-of-partitions-after-operations/solutions/2595072/ji-yi-hua-sou-suo-jian-ji-xie-fa-pythonj-6g5z/
+    private final Map<Long, Integer> memo = new HashMap<>();
+
+    public int maxPartitionsAfterOperations(String s, int k) {
+        return dfs(0, 0, 0, s.toCharArray(), k);
+    }
+
+    private int dfs(int i, int mask, int changed, char[] s, int k) {
+        if (i == s.length) {
+            return 1;
+        }
+
+        long argsMask = (long) i << 32 | mask << 1 | changed;
+        if (memo.containsKey(argsMask)) { // 之前計算過
+            return memo.get(argsMask);
+        }
+
+        int res;
+        // 不改 s[i]
+        int bit = 1 << (s[i] - 'a');
+        int newMask = mask | bit;
+        if (Integer.bitCount(newMask) > k) {
+            // 分割出一個子串，這個子串的最後一個字母在 i-1
+            // s[i] 作為下一段的第一個字母，也就是 bit 作為下一段的 mask 的初始值
+            res = dfs(i + 1, bit, changed, s, k) + 1;
+        } else { // 不分割
+            res = dfs(i + 1, newMask, changed, s, k);
+        }
+
+        if (changed == 0) {
+            // 枚舉把 s[i] 改成 a,b,c,...,z
+            for (int j = 0; j < 26; j++) {
+                newMask = mask | (1 << j);
+                if (Integer.bitCount(newMask) > k) {
+                    // 分割出一個子串，這個子串的最後一個字母在 i-1
+                    // j 作為下一段的第一個字母，也就是 1<<j 作為下一段的 mask 的初始值
+                    res = Math.max(res, dfs(i + 1, 1 << j, 1, s, k) + 1);
+                } else { // 不分割
+                    res = Math.max(res, dfs(i + 1, newMask, 1, s, k));
+                }
+            }
+        }
+
+        memo.put(argsMask, res); // 記憶化
+        return res;
+    }
 }
